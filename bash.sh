85f5fc53e5159aa8e5bc65fc36563a2de3e97c2d
林楨祥 <z9027606@gmail.com>
2022-08-27 21:09:59 +0800

    add_env

--- bash.sh
+++ bash.sh
@@ -5,4 +5,4 @@ docker rm node-app
 docker rmi node-app-image
 #重新建立image並且重啟containers
 docker build -t  node-app-image .
-docker run -v :/app -p 3000:3000 -d --name node-app node-app-image
+docker run -v :/app -v /app/node_modules --env PORT=4000 -p 3000:4000 -d --name node-app node-app-image
